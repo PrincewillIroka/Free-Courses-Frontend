d0ced500f1fb883500f9f3876a0d17b3e74ec698
Princewill Iroka <princewilliroka@outlook.com>
2021-03-06 16:11:57 +0100

    Updated Tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ["./store/**/*.js"],
+  purge: [],
   prefix: "",
   important: false,
   separator: ":",
